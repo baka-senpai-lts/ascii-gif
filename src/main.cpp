1e8273a8acf327d8680cec9c4f3ed03694b6b0b9
baka-senpai-lts <baka.senpai.lts@gmail.com>
2021-01-19 00:01:50 +0200

    Add help

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,25 @@
 #include "Reader.hpp"
 #include "SequenceGenerator.hpp"
 
+
+const char* HELP = "USAGE\n\t"
+                   "ascii-gif [options] [file]\n\n"
+                   "DESCRIPTION\n\t"
+                   "Utility to convert video to ASCII Art sequence\n\n"
+                   "OPTIONS\n\t"
+                   "-h --help\tShow this\n";
+
 int main(int argc, char* argv[])
 {
+    ArgsParser parser(argv, argv+argc);
+
+    if(parser.exists("-h") || parser.exists("--help"))
+    {
+        std::cout << HELP;
+    }
+
+    // It crashes everything, so for now I'll take it away
+    //delete &parser;
+
     return 0;
 }
